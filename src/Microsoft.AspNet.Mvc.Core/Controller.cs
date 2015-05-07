c5c26ecf104f4c6ff927b60ff7353573a577e12a
Harsh Gupta <harshg@microsoft.com>
2015-05-07 15:35:40 -0700

    Collections with custom indexes working. Need to fix Body And FormFile/FormCollection validations.

--- src/Microsoft.AspNet.Mvc.Core/Controller.cs
+++ src/Microsoft.AspNet.Mvc.Core/Controller.cs
@@ -1311,7 +1311,9 @@ namespace Microsoft.AspNet.Mvc
                 modelState: ModelState,
                 modelExplorer: modelExplorer);
 
-            ObjectValidator.Validate(validationContext, new ModelValidationNode(modelName, modelExplorer.Metadata));
+            ObjectValidator.Validate(
+                validationContext,
+                new ModelValidationNode(modelName, modelExplorer.Metadata, model));
             return ModelState.IsValid;
         }
 
